8def5b88583a9985cccd40bafe2bb397fa164160
Ananth Sriram <87041264+ananthsriram1@users.noreply.github.com>
2026-09-09 17:01:07 -0400

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+
+        null_byte = "\x00"
+        rv = ""
+
+        for s in strs:
+            rv += s
+            rv += null_byte
+
+        return rv
+
+    def decode(self, s: str) -> List[str]:
+
+        rv = []
+
+        index = 0
+
+        while index < len(s):
+            count = index
+            curr_word = ""
+            while count < len(s):
+                if s[count] == "\x00":
+                    count += 1
+                    break
+                else:
+                    curr_word += s[count]
+                    count += 1
+            index = count
+            rv.append(curr_word) 
+            
+        return rv
